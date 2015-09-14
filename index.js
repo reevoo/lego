eee03282aa39051f09b36940b8d5da87c49867e3
Victor Vila <victor.vila@reevoo.com>
2015-09-14 16:46:34 +0100

    Added Form component.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // Component imports
 import _Button from './src/button/button';
 import _CheckBox from './src/check_box/check_box';
+import _Form from './src/form/form';
 import _TextBox from './src/text_box/text_box';
 
 // Export components
 export const Button = _Button;
 export const CheckBox = _CheckBox;
+export const Form = _Form;
 export const TextBox = _TextBox;
---
+++ src/form/form.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import BaseComponent from '../base-component';
+import Button from '../button/button';
+
+export default class Form extends BaseComponent {
+  render() {
+    let title = this.props.title ? <h4>{this.props.title}</h4> : null;
+    let submitLabel = this.props.submitLabel ? this.props.submitLabel : 'Send';
+
+    return (
+      <form className='card-form' onSubmit={this.props.onSubmit}>
+        {title}
+        {this.props.children}
+        <Button type='submit'>{submitLabel}</Button>
+      </form>
+    );
+  }
+}
+
+Form.propTypes = {
+  onSubmit: React.PropTypes.func.isRequired,
+  submitLabel: React.PropTypes.string,
+  title: React.PropTypes.string,
+};
