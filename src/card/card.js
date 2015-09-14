87e9a304b225d7b2c79677ec17bed041bf144415
Victor Vila <victor.vila@reevoo.com>
2015-09-14 17:32:50 +0100

    Added Card component.

---
+++ src/card/card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class Card extends React.Component {
+  render() {
+    return (
+      <div className={`card mdl-shadow--2dp mdl-color--white mdl-cell mdl-cell--${this.props.cols}-col ${this.props.className}`}
+        onClick={this.props.onClick}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+Card.propTypes = {
+  cols: React.PropTypes.number,
+};
+
+Card.defaultProps = {
+  cols: 12,
+};
