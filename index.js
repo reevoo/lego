71e249952008a267459938ca5927635e9c144989
Victor Vila <victor.vila@reevoo.com>
2015-09-14 17:37:17 +0100

    Exported new components.

--- index.js
+++ index.js
@@ -2,22 +2,28 @@
 import _Autocomplete from './src/autocomplete/autocomplete';
 import _AutocompleteTagList from './src/autocomplete/autocomplete_tag_list';
 import _Button from './src/button/button';
+import _Card from './src/card/card';
 import _CheckBox from './src/check_box/check_box';
 import _Dropdown from './src/dropdown/dropdown';
 import _DropdownTagList from './src/dropdown/dropdown_tag_list';
 import _Form from './src/form/form';
 import _IconButton from './src/icon_button/icon_button';
+import _Sidebar from './src/sidebar/sidebar';
 import _TagList from './src/tag_list/tag_list';
 import _TextBox from './src/text_box/text_box';
+import _Topbar from './src/topbar/topbar';
 
 // Export components
 export const Autocomplete = _Autocomplete;
 export const AutocompleteTagList = _AutocompleteTagList;
 export const Button = _Button;
+export const Card = _Card;
 export const CheckBox = _CheckBox;
 export const Dropdown = _Dropdown;
 export const DropdownTagList = _DropdownTagList;
 export const Form = _Form;
 export const IconButton = _IconButton;
+export const Sidebar = _Sidebar;
 export const TagList = _TagList;
 export const TextBox = _TextBox;
+export const Topbar = _Topbar;
