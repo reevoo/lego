c9f21aae8f13218ad460afe8e543cb1ac88a394a
Victor Vila <victor.vila@reevoo.com>
2015-09-14 16:44:36 +0100

    Added CheckBox component.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Component imports
 import _Button from './src/button/button';
+import _CheckBox from './src/check_box/check_box';
 import _TextBox from './src/text_box/text_box';
 
 // Export components
 export const Button = _Button;
+export const CheckBox = _CheckBox;
 export const TextBox = _TextBox;
---
+++ src/check_box/check_box.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import BaseComponent from '../base-component';
+
+const DEFAULT_TYPE = 'checkbox'; // switch is the other option
+
+export default class CheckBox extends BaseComponent {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { checked: props.checked };
+
+    this._bind('handleChange');
+  }
+
+  handleChange(event) {
+    this.setState({checked: event.target.checked});
+  }
+
+  render() {
+    let type = this.props.type ? this.props.type : DEFAULT_TYPE;
+
+    return (
+      <label className={`mdl-${type} mdl-js-${type} mdl-js-ripple-effect`} for={this.props.id}>
+        <input
+          id={this.props.id}
+          className={`mdl-${type}__input`}
+          onChange={this.handleChange}
+          type='checkbox'
+          checked={this.state.checked}
+           />
+         <span className={`mdl-${type}__label`}>{this.props.label}</span>
+      </label>
+    );
+  }
+
+}
+
+CheckBox.propTypes = {
+  checked: React.PropTypes.bool,
+  id: React.PropTypes.string,
+  label: React.PropTypes.string.isRequired,
+  type: React.PropTypes.oneOf([DEFAULT_TYPE, 'switch']),
+};
