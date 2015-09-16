fe48669f36b25a9a00e9fd9e81a38e3ac830ff90
Victor Vila <victor.vila@reevoo.com>
2015-09-16 16:14:18 +0100

    Minor fix on TableRow.

--- src/table/table_row.js
+++ src/table/table_row.js
@@ -5,6 +5,8 @@ import TableCell from './table_cell';
 export default class TableRow extends React.Component {
 
   render() {
+    let item = this.props.item;
+
     return (
       <tr>
         {this.props.columns.map(column =>
@@ -19,7 +21,7 @@ export default class TableRow extends React.Component {
 }
 
 TableRow.propTypes = {
-  items: React.PropTypes.array.isRequired,
+  item: React.PropTypes.array.isRequired,
   columns: React.PropTypes.arrayOf(React.PropTypes.shape({
     key: React.PropTypes.string.isRequired,
     content: React.PropTypes.func.isRequired,
